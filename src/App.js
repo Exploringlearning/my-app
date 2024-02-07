9e08aade7a7ae635685fb7d75716afc3aa73e1a2
Swapnil <swapnil.gupta@thoughtworks.com>
2024-02-07 16:16:15 +0530

    Datat types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "TestApp";
+  const age = 20
+  const likes = ["Apple", "Orange", "Pineapple"];
+
   return (
-    <div className="App">
-  <h1>Hello World !!</h1>
+    <div id="app-id">
+  <h1 id ="header-id">Hello {name} !!</h1>
+  <h2>{age}</h2>
+  <h2>{likes}</h2>
+  <h2>{String(true)}</h2>
+  {
+    true ? <h3>New TestApp</h3> : null
+  }
+  {
+   likes.map(like => <h2 key = {like}> {like}</h2>)
+  }
+
     </div>
   );
 }
